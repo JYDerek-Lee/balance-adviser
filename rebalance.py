de2ebc48a319d5bdb4ddd1178b8c8ccbefbfad7a
Window10 <asplannedl@gmail.com>
2020-06-13 14:46:18 +0900

    Fixed lint by black

--- rebalance.py
+++ rebalance.py
@@ -1,52 +1,67 @@
 import math
 import numpy as np
 
+
 def assetsDict(balance):
     for k, v in balance.items():
-        v['value'] = v['amount'] * v['price']
+        v["value"] = v["amount"] * v["price"]
     return balance
+
 
 def normalize(balance):
     total = 0
     for k, v in balance.items():
-        total += v 
+        total += v
     ret = dict()
     for k, v in balance.items():
         ret[k] = float(v) / float(total)
     return ret
 
+
 def amounts(balance):
-    return [ v['amount'] for k, v in balance.items() ]
+    return [v["amount"] for k, v in balance.items()]
+
 
 def prices(balance):
-    return [ v['price'] for k, v in balance.items() ]
+    return [v["price"] for k, v in balance.items()]
+
 
 def ratios(balance):
-    return [ float(v['ratio']) for k, v in balance.items() ]
+    return [float(v["ratio"]) for k, v in balance.items()]
+
 
 def rebalance(balance):
     assets = np.multiply(amounts(balance), prices(balance))
     normAssets = assets / np.abs(assets).sum()
     normRatios = ratios(balance) / np.abs(ratios(balance)).sum()
-    sellRatios = list(map(lambda a, r: (a - r)/a if a > r else 0, normAssets, normRatios))
-    buyRatios  = list(map(lambda a, r: (r - a)/a if r > a else 0, normAssets, normRatios))
-    keys = [ k for k, v in balance.items()]
-    sellAmounts = dict(zip(keys, 
-        list(map(lambda a, r: math.floor(a * r), amounts(balance), sellRatios))
-        ))
-    buyAmounts  = dict(zip(keys, 
-        list(map(lambda a, r: math.floor(a * r), amounts(balance), buyRatios))
-        ))
+    sellRatios = list(
+        map(lambda a, r: (a - r) / a if a > r else 0, normAssets, normRatios)
+    )
+    buyRatios = list(
+        map(lambda a, r: (r - a) / a if r > a else 0, normAssets, normRatios)
+    )
+    keys = [k for k, v in balance.items()]
+    sellAmounts = dict(
+        zip(
+            keys,
+            list(map(lambda a, r: math.floor(a * r), amounts(balance), sellRatios)),
+        )
+    )
+    buyAmounts = dict(
+        zip(
+            keys, list(map(lambda a, r: math.floor(a * r), amounts(balance), buyRatios))
+        )
+    )
     change = 0
-    balanceExceptCash = dict(filter(lambda elem: elem[0] != 'cash', balance.items()))
+    balanceExceptCash = dict(filter(lambda elem: elem[0] != "cash", balance.items()))
     for k, v in balanceExceptCash.items():
-        v['amount'] -= sellAmounts[k]
-        change += (v['price'] * sellAmounts[k])
-    change += balance['cash']['amount']
+        v["amount"] -= sellAmounts[k]
+        change += v["price"] * sellAmounts[k]
+    change += balance["cash"]["amount"]
     for k, v in balanceExceptCash.items():
-        ableAmount = math.floor(change / v['price'])
+        ableAmount = math.floor(change / v["price"])
         amount = buyAmounts[k] if buyAmounts[k] <= ableAmount else ableAmount
-        v['amount'] += amount
-        change -= v['price'] * amount
-    balance['cash']['amount'] = change
+        v["amount"] += amount
+        change -= v["price"] * amount
+    balance["cash"]["amount"] = change
     return balance
--- run.py
+++ run.py
@@ -3,58 +3,87 @@ from matplotlib import pyplot as plt
 from rebalance import *
 from envelope import *
 
-snp = data.DataReader('^GSPC', 'yahoo', start='2003-01-02')
-treas = data.DataReader('TLT', 'yahoo', start='2003-01-02')
+snp = data.DataReader("^GSPC", "yahoo", start="2003-01-02")
+treas = data.DataReader("TLT", "yahoo", start="2003-01-02")
 
 initialCash = 1000000
 
-balanceOnlyStock = {
-        "stock": { "price": 0, "amount": 0 }
-        }
-balanceOnlyStock["stock"]["price"] = snp['Close'][0]
-balanceOnlyStock["stock"]["amount"] = math.floor(initialCash / balanceOnlyStock["stock"]["price"])
+balanceOnlyStock = {"stock": {"price": 0, "amount": 0}}
+balanceOnlyStock["stock"]["price"] = snp["Close"][0]
+balanceOnlyStock["stock"]["amount"] = math.floor(
+    initialCash / balanceOnlyStock["stock"]["price"]
+)
 
 balanceStockAndBond = []
 for i in range(0, 9):
-    balanceStockAndBond.append( {
-        "bond": { "price": 0, "amount": 0, "ratio": (10 - i) },
-        "cash": { "price": 1, "amount": 0, "ratio": 0 }, 
-        "stock": { "price": 0, "amount": 0, "ratio": i }
-        } )
-    ratios = [float(v['ratio']) for k, v in balanceStockAndBond[i].items()]
-    normRatios = ratios / np.abs(ratios).sum()
-    balanceStockAndBond[i]["stock"]["price"] = snp['Close'][0]
-    balanceStockAndBond[i]["stock"]["amount"] = math.floor((normRatios[2] * initialCash) / balanceStockAndBond[i]["stock"]["price"])
-    balanceStockAndBond[i]["bond"]["price"] = treas['Close'][0]
-    balanceStockAndBond[i]["bond"]["amount"] = math.floor((normRatios[0] * initialCash) / balanceStockAndBond[i]["bond"]["price"])
-dynamicBalance = { "bond": { "price": 0, "amount": 0, "ratio": 2 },
-        "cash": { "price": 1, "amount": 0, "ratio": 0 }, 
-        "stock": { "price": 0, "amount": 0, "ratio": 8 }
+    balanceStockAndBond.append(
+        {
+            "bond": {"price": 0, "amount": 0, "ratio": (10 - i)},
+            "cash": {"price": 1, "amount": 0, "ratio": 0},
+            "stock": {"price": 0, "amount": 0, "ratio": i},
         }
-dynamicBalance["stock"]["price"] = snp['Close'][0]
-dynamicBalance["stock"]["amount"] = math.floor((normRatios[2] * initialCash) / dynamicBalance["stock"]["price"])
-dynamicBalance["bond"]["price"] = treas['Close'][0]
-dynamicBalance["bond"]["amount"] = math.floor((normRatios[0] * initialCash) / dynamicBalance["bond"]["price"])
+    )
+    ratios = [float(v["ratio"]) for k, v in balanceStockAndBond[i].items()]
+    normRatios = ratios / np.abs(ratios).sum()
+    balanceStockAndBond[i]["stock"]["price"] = snp["Close"][0]
+    balanceStockAndBond[i]["stock"]["amount"] = math.floor(
+        (normRatios[2] * initialCash) / balanceStockAndBond[i]["stock"]["price"]
+    )
+    balanceStockAndBond[i]["bond"]["price"] = treas["Close"][0]
+    balanceStockAndBond[i]["bond"]["amount"] = math.floor(
+        (normRatios[0] * initialCash) / balanceStockAndBond[i]["bond"]["price"]
+    )
+dynamicBalance = {
+    "bond": {"price": 0, "amount": 0, "ratio": 2},
+    "cash": {"price": 1, "amount": 0, "ratio": 0},
+    "stock": {"price": 0, "amount": 0, "ratio": 8},
+}
+dynamicBalance["stock"]["price"] = snp["Close"][0]
+dynamicBalance["stock"]["amount"] = math.floor(
+    (normRatios[2] * initialCash) / dynamicBalance["stock"]["price"]
+)
+dynamicBalance["bond"]["price"] = treas["Close"][0]
+dynamicBalance["bond"]["amount"] = math.floor(
+    (normRatios[0] * initialCash) / dynamicBalance["bond"]["price"]
+)
 
-onlyStock = [ (balanceOnlyStock["stock"]["price"] * balanceOnlyStock["stock"]["amount"]) ]
+onlyStock = [(balanceOnlyStock["stock"]["price"] * balanceOnlyStock["stock"]["amount"])]
 staticPortfolio = []
 dynamicPortfolio = []
 stock = []
 bond = []
 cash = []
 for i in range(0, 9):
-    staticPortfolio.append([ (balanceStockAndBond[i]["stock"]["price"] * balanceStockAndBond[i]["stock"]["amount"]) + 
-            (balanceStockAndBond[i]["bond"]["price"] * balanceStockAndBond[i]["bond"]["amount"]) ])
+    staticPortfolio.append(
+        [
+            (
+                balanceStockAndBond[i]["stock"]["price"]
+                * balanceStockAndBond[i]["stock"]["amount"]
+            )
+            + (
+                balanceStockAndBond[i]["bond"]["price"]
+                * balanceStockAndBond[i]["bond"]["amount"]
+            )
+        ]
+    )
     stock.append([balanceStockAndBond[i]["stock"]["amount"]])
     bond.append([balanceStockAndBond[i]["bond"]["amount"]])
     cash.append([balanceStockAndBond[i]["cash"]["amount"]])
-dynamicPortfolio.append((balanceStockAndBond[i]["stock"]["price"] * balanceStockAndBond[i]["stock"]["amount"]) + 
-            (balanceStockAndBond[i]["bond"]["price"] * balanceStockAndBond[i]["bond"]["amount"]))
+dynamicPortfolio.append(
+    (
+        balanceStockAndBond[i]["stock"]["price"]
+        * balanceStockAndBond[i]["stock"]["amount"]
+    )
+    + (
+        balanceStockAndBond[i]["bond"]["price"]
+        * balanceStockAndBond[i]["bond"]["amount"]
+    )
+)
 
-for s, b in zip(snp['Close'], treas['Close']):
+for s, b in zip(snp["Close"], treas["Close"]):
     for i in range(0, 9):
-        balanceStockAndBond[i]['stock']['price'] = s
-        balanceStockAndBond[i]['bond']['price'] = b
+        balanceStockAndBond[i]["stock"]["price"] = s
+        balanceStockAndBond[i]["bond"]["price"] = b
         balanceStockAndBond[i] = rebalance(balanceStockAndBond[i])
         stockVal = s * balanceStockAndBond[i]["stock"]["amount"]
         bondVal = b * balanceStockAndBond[i]["bond"]["amount"]
@@ -63,8 +92,8 @@ for s, b in zip(snp['Close'], treas['Close']):
         stock[i].append(balanceStockAndBond[i]["stock"]["amount"])
         bond[i].append(balanceStockAndBond[i]["bond"]["amount"])
         cash[i].append(balanceStockAndBond[i]["cash"]["amount"])
-    dynamicBalance['stock']['price'] = s
-    dynamicBalance['bond']['price'] = b
+    dynamicBalance["stock"]["price"] = s
+    dynamicBalance["bond"]["price"] = b
     dynamicBalance = rebalance(dynamicBalance)
     stockVal = s * dynamicBalance["stock"]["amount"]
     bondVal = b * dynamicBalance["bond"]["amount"]
@@ -75,15 +104,15 @@ for s, b in zip(snp['Close'], treas['Close']):
 print(snp)
 print(treas)
 
-plt.subplot(2,1,1)
-plt.plot(staticPortfolio[5], label='Static Portfolio')
-plt.plot(dynamicPortfolio, label='Dynamic Portfolio')
-plt.plot(onlyStock, label='Only Stock')
-plt.legend(loc='upper left')
-plt.subplot(2,1,2)
-plt.plot(snp['Close'], label='Stock')
-plt.plot(snp['Close'].ewm(50).mean(), label='EMA')
-plt.plot(envelopeHiBounds(snp['Close'], 50, 0.1), label='upper')
-plt.plot(envelopeLoBounds(snp['Close'], 50, 0.1), label='lower')
-plt.legend(loc='upper left')
+plt.subplot(2, 1, 1)
+plt.plot(staticPortfolio[5], label="Static Portfolio")
+plt.plot(dynamicPortfolio, label="Dynamic Portfolio")
+plt.plot(onlyStock, label="Only Stock")
+plt.legend(loc="upper left")
+plt.subplot(2, 1, 2)
+plt.plot(snp["Close"], label="Stock")
+plt.plot(snp["Close"].ewm(50).mean(), label="EMA")
+plt.plot(envelopeHiBounds(snp["Close"], 50, 0.1), label="upper")
+plt.plot(envelopeLoBounds(snp["Close"], 50, 0.1), label="lower")
+plt.legend(loc="upper left")
 plt.show()
